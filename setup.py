f5f8c84159a599dd6b4e58d1047018be6064b0d6
mattdickers <mattdickers@gmail.com>
2024-01-22 14:25:13 +0100

    Added all files needed for upload to PyPi

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'MBN_tools',)
+  packages = ['MBN_tools'],
+  version = '0.1',
+  license='MIT',
+  description = 'Useful tools for MBN Explorer simulations and analysis',
+  author = 'Matthew Dickers',
+  author_email = 'mattdickers@gmail.com',
+  url = 'https://github.com/mattdickers/MBN_tools',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['MBN Explorer', 'MBN Studio', 'Molecular Dynamics', 'MD', 'Data Analysis'],
+  install_requires=[
+          'numpy',
+          'mdtraj',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Researchers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
